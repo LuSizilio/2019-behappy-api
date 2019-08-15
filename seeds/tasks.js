054a7c4eed22f50644808165da4fd1a52f7efa19
LuSizilio <lucianorsizilio@gmail.com>
2019-08-15 10:13:42 -0300

    ok

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -1,9 +1,11 @@
+const table_name = "tasks";
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('tasks').del()
+  return knex(table_name).del()
     .then(function () {
       // Inserts seed entries
-      return knex('tasks').insert([
+      return knex(table_name).insert([
         {
           title: 'Bom dia', 
           description: 'Você deu bom dia para alguém há 44 dias', 
